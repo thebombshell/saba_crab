3b8b3a24627f9c934e2b052f43f4d60774632adb
Scott R Howell <thebombshell@live.co.uk>
2025-07-06 13:21:43 +0100

    ambience regions work now

--- scripts/ambience_tracker.gd
+++ scripts/ambience_tracker.gd
@@ -1,7 +1,6 @@
 class_name AmbienceTrackerActor extends Node3D
 
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D;
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 @export var listener_node: Node3D = null;
 @export var ambience_names: Dictionary[String, int] = {};
@@ -20,13 +19,11 @@ func get_fade_distance(t_name: String):
 	
 	return ambience_fade_distance[t_name] if ambience_fade_distance.has(t_name) else default_fade_distance;
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(t_delta: float) -> void:
 	
 	# need a listener to reference
 	if !is_instance_valid(listener_node):
 		return;
-	
-	mesh_instance_3d.global_position = (get_node("Sea/CollisionPolygon3D").get_closest_point(listener_node.global_position));
 	
 	# need a
 	if audio_stream_player_3d.stream is not AudioStreamSynchronized:
@@ -38,10 +35,15 @@ func _physics_process(_delta: float) -> void:
 			continue;
 		var area: Area3D = get_node(t_name);
 		var id = ambience_names[t_name];
-		if area.has_overlapping_bodies():
+		if area.has_overlapping_areas():
 			stream.set_sync_stream_volume(id, 0.0);
 		else:
 			var alpha = clamp(smoothstep(0.0, get_fade_distance(t_name),
 				find_nearest_distance(area)), 0.0, 1.0);
-			stream.set_sync_stream_volume(id, lerp(0.0, -60.0, alpha));
+			var value = alpha * -60.0;
+			var old_value = stream.get_sync_stream_volume(id);
+			if value <= -60.0:
+				stream.set_sync_stream_volume(id, old_value - t_delta);
+			else:
+				stream.set_sync_stream_volume(id, value);
 	return;
--- scripts/point_check_collision_polygon.gd
+++ scripts/point_check_collision_polygon.gd
@@ -6,8 +6,8 @@ func _ready():
 	
 	for vertex in polygon:
 		
-		vertices.append(to_global(Vector3(vertex.x, vertex.y, 0.0)));
-		#vertices.append(to_global(Vector3(vertex.x, vertex.y, -depth)));
+		vertices.append(to_global(Vector3(vertex.x, vertex.y, -depth)));
+		vertices.append(to_global(Vector3(vertex.x, vertex.y, +depth)));
 	return;
 
 func get_closest_point(t_point: Vector3) -> Vector3:
@@ -15,20 +15,14 @@ func get_closest_point(t_point: Vector3) -> Vector3:
 	var point = Vector3.ZERO;
 	var dist = 1000000.0;
 	for i in polygon.size() - 1:
-		var a = Geometry3D.get_closest_point_to_segment(t_point, vertices[i], vertices[i + 1]);
-		var new_dist = a.distance_to(t_point);
-		if i < 0:
-			var b = Geometry3D.get_closest_point_to_segment(t_point, vertices[i], vertices[i + 2]);
-			var b_dist = b.distance_to(t_point);
-			if b_dist < new_dist:
-				a = b;
-				new_dist = b_dist;
-			b = Geometry3D.get_closest_point_to_segment(t_point, vertices[i + 1], vertices[i + 3]);
-			b_dist = b.distance_to(t_point);
-			if b_dist < new_dist:
-				a = b;
-				new_dist = b_dist;
+		var x = i * 2;
+		var a = Geometry3D.get_closest_point_to_segment(t_point,
+			vertices[(x + 0) % vertices.size()], vertices[(x + 2) % vertices.size()]);
+		var b = Geometry3D.get_closest_point_to_segment(t_point,
+			vertices[(x + 1) % vertices.size()], vertices[(x + 3) % vertices.size()]);
+		var c = Geometry3D.get_closest_point_to_segment(t_point, a, b);
+		var new_dist = c.distance_to(t_point);
 		if new_dist < dist:
-			point = a;
+			point = c;
 			dist = new_dist;
 	return point;
