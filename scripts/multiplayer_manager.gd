40f462fab7f71b80f5adc9c420b99f4b29bd6d7f
Scott R Howell <thebombshell@live.co.uk>
2025-07-08 04:53:56 +0100

    the back and forth of change, test, change , test begins

--- scripts/multiplayer_manager.gd
+++ scripts/multiplayer_manager.gd
@@ -21,6 +21,7 @@ func init_multiplayer_internal(t_port: int = 60123):
 	
 	var peer = ENetMultiplayerPeer.new();
 	peer.create_server(t_port, 8);
+	listen_to_peer(peer);
 	tree_multiplayer.multiplayer_peer = peer;
 	return;
 
@@ -28,6 +29,7 @@ func join_multiplayer_internal(t_ip: String, t_port: int = 60123):
 	
 	var peer = ENetMultiplayerPeer.new();
 	peer.create_client(t_ip, t_port);
+	listen_to_peer(peer);
 	tree_multiplayer.multiplayer_peer = peer;
 	return;
 
@@ -35,10 +37,39 @@ func end_multiplayer_internal():
 	
 	tree_multiplayer.multiplayer_peer = null;
 	return;
+	
+func _connected_to_server():
+	
+	CommandPanel.add_line("MM", "Connected to server!");
+	return;
+
+func _connection_failed():
+	
+	CommandPanel.add_line("MM", "Failed to connect to server!");
+	return;
+
+func _peer_connected(t_id: int):
+	
+	CommandPanel.add_line("MM", "Peer %d has connected!" % t_id);
+	return
+
+func _peer_disconnected(t_id: int):
+	
+	CommandPanel.add_line("MM", "Peer %d has disconnected!" % t_id);
+	return
+
+func listen_to_peer(t_peer: MultiplayerPeer):
+	
+	t_peer.peer_connected.connect(_peer_connected);
+	t_peer.peer_disconnected.connect(_peer_disconnected);
+	return;
 
 func _ready():
 	
 	if is_instance_valid(current):
 		push_error("There should only be a single MultiplayerManager at a time");
 	current = self;
+	
+	tree_multiplayer.connected_to_server.connect(_connected_to_server);
+	tree_multiplayer.connection_failed.connect(_connection_failed);
 	return;
